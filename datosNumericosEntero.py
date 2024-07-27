5d1b31d02f052d11bb83796b90345b817f51c70a
Ali_Hernandez <burbujalia2@outlook.com>
2024-07-27 13:54:36 -0600

    se agrego archivo datos

---
+++ datosNumericosEntero.py
@@ -0,0 +1,10 @@
+a = 10
+b = 11
+c = 8 
+print ("Valor de a: ", a)
+print ("El tipo de dato de a es:", type(a))
+print ("El valor de b", b)
+print ("el tipo de dato de b es", type(b))
+
+print ("valor de c:", c)
+print ("el tipo de dato de c es", type(c))
